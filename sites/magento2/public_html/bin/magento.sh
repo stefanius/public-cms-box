6cda7608345f5db7a810c604c637b8ca9a0eaaa3
stefanius <KIT_kat_16>
2015-05-24 15:50:38 +0200

    Updated magento2 install script

--- sites/magento2/public_html/bin/magento.sh
+++ sites/magento2/public_html/bin/magento.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 sudo apt-get install mysql-server-5.6
+sudo apt-get install php5-gd
+sudo apt-get install php5-mcrypt
+sudo apt-get install php5-intl
+cd /home/vagrant/sites/magento2/public_html
+composer install
 export PATH=$PATH:/home/vagrant/sites/magento2/bin
 echo "SHOW VARIABLES LIKE \"%version%\";" | mysql -uroot -ppassword
 echo "create database magento2" | mysql -uroot -ppassword
+cd /home/vagrant/sites/magento2/public_html/bin
 php magento setup:install --db_password password --db_user root --admin_user admin --admin_password admin --admin_email admin@admin.nl --admin_firstname henkie --admin_lastname spenkie
